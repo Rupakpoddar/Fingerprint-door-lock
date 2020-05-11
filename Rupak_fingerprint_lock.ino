bd0313247a90d736679f2ac13e99b1e255f0aeb6
Rupak Poddar <34826905+Rupakpoddar@users.noreply.github.com>
2020-05-11 10:39:47 +0530

    Update Rupak_fingerprint_lock.ino

--- Rupak_fingerprint_lock.ino
+++ Rupak_fingerprint_lock.ino
@@ -9,43 +9,100 @@
  *  Rx(white)---------------> Pin 3
  */
 
- // Connect the Door Lock on digital pin 12
- 
 #include <Adafruit_Fingerprint.h>
-int electronic_lock = 12; 
+
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
-SoftwareSerial mySerial(2, 3);// Tx, Rx
+SoftwareSerial mySerial(2, 3);
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
+int door_lock = 12; //connect the door lock to pin 12
+int lock_delay = 1500; //change the delay from here
+
 void setup()  
 {
-  pinMode(12, OUTPUT);
+  pinMode(door_lock, OUTPUT);
+  digitalWrite(door_lock, LOW);
   Serial.begin(9600);
-  while (!Serial);
-  delay(100);
-  // set the data rate for the sensor serial port
   finger.begin(57600);
-  
-  if (finger.verifyPassword()) {
-    Serial.println("Found fingerprint sensor");
-  } else {
-    Serial.println("Did not find fingerprint sensor");
-    while (1) { delay(1); }
-  }
-
-  finger.getTemplateCount();
-  Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" template(s)");
-  Serial.println("Waiting for valid finger...");
 }
 
-void loop()             // run over and over again
+void loop()
 {
   getFingerprintIDez();
-  delay(50);            //don't ned to run this at full speed.
+  delay(50);
 }
 
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+  digitalWrite(door_lock, HIGH);
+  delay(lock_delay);
+  digitalWrite(door_lock, LOW);
+  return finger.fingerID;
+}
+
+// returns -1 if failed, otherwise returns ID #
 int getFingerprintIDez() {
   uint8_t p = finger.getImage();
   if (p != FINGERPRINT_OK)  return -1;
@@ -59,9 +116,8 @@ int getFingerprintIDez() {
   // found a match!
   Serial.print("Found ID #"); Serial.print(finger.fingerID); 
   Serial.print(" with confidence of "); Serial.println(finger.confidence);
-  digitalWrite(electronic_lock, HIGH);
-  delay(1500);                        //Change the door lock delay from here
-  digitalWrite(electronic_lock, LOW);
-  Serial.println("Unlocked");
+  digitalWrite(door_lock, HIGH);
+  delay(lock_delay);
+  digitalWrite(door_lock, LOW);
   return finger.fingerID; 
 }
